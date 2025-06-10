cfcd78bc6fec5a2314a3616d5215f1255701a35a
romaperec <cpser621@gmail.com>
2025-06-10 15:58:37 +0300

    Added email validator

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,4 +1,5 @@
 from fastapi import HTTPException, status
+from email_validator import validate_email, EmailNotValidError
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -9,6 +10,12 @@ from src.auth.utils import hash_password
 
 
 async def register_user(user_schema: UserSchema, db: AsyncSession):
+    try:
+        valid = validate_email(user_schema.email)
+        email_normalized = valid.email
+    except EmailNotValidError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
     user_in_db = await db.execute(
         select(UserModel).where(user_schema.email == UserModel.email)
     )
@@ -20,7 +27,7 @@ async def register_user(user_schema: UserSchema, db: AsyncSession):
         )
 
     hashed_password = hash_password(user_schema.password)
-    new_user = UserModel(email=user_schema.email, password=hashed_password)
+    new_user = UserModel(email=email_normalized, password=hashed_password)
 
     db.add(new_user)
     await db.commit()
