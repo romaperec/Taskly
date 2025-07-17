6c4a7cbb415d676c4bb0d8f4f80a33adad4dce09
romaperec <cpser621@gmail.com>
2025-07-17 03:07:02 +0300

    Fix get .env variables

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class DatabaseConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
     DB_URL: str = "postgresql+asyncpg://name:pass@localhost:5432/dbname"
     DB_ECHO: bool = False
 
